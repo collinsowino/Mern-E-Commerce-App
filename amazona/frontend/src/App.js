20a7221a344ead8feece5bdf43f2b0080bca4c01
collinsowino <scolow2111@ueab.ac.ke>
2022-09-15 16:00:18 +0300

    Video-3-Create-ReactApp

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+};
+
+export default App;
